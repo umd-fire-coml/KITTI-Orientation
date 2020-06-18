15d7e24c8f97d33e2761b1bb330ad0a6196172c6
Raymond H. Tu <hh2@umd.edu>
2020-06-18 17:27:59 -0400

    Added code comments

--- data_processing.py
+++ data_processing.py
@@ -12,15 +12,22 @@ VEHICLES = ['Car', 'Truck', 'Van', 'Tram','Pedestrian','Cyclist']
 
 
 def compute_anchors(angle):
+    # angle is the new_alpha angle between 0 and 2pi
     anchors = []
+    wedge = 2.*np.pi/BIN # angle size of each bin, i.e. 180 deg
+    l_index = int(angle/wedge) # round down with int, tells me which bin the angle belongs to, gets to be either 0 or 1
+    r_index = l_index + 1 # get to be either 1 or 2
     
-    wedge = 2.*np.pi/BIN
-    l_index = int(angle/wedge)
-    r_index = l_index + 1
-    
+    # (angle - l_index*wedge) is the +offset angle from start of the wedge, l_index*wedge is either 0 or 180
+    # wedge/2 * (1+OVERLAP/2) is 90 deg * 1.05
+    # basically check if the angle is within majority part of the current wedge
     if (angle - l_index*wedge) < wedge/2 * (1+OVERLAP/2):
+        # append the bin index of the angle, and the +offset angle from start of the wedge
         anchors.append([l_index, angle - l_index*wedge])
-        
+    
+    # r_index*wedge - angle is the -offset angle from start of the next wedge, r_index*wedge is either 180 or 360 deg
+    # wedge/2 * (1+OVERLAP/2) is 90 deg * 1.05
+    # basically check if the angle is also within majority part of the next wedge
     if (r_index*wedge - angle) < wedge/2 * (1+OVERLAP/2):
         anchors.append([r_index%BIN, angle - r_index*wedge])
         
@@ -40,9 +47,13 @@ def parse_annotation(label_dir, image_dir):
             occluded  = np.abs(float(line[2]))
 
             if line[0] in VEHICLES and truncated < 0.1 and occluded < 0.1:
-                new_alpha = float(line[3]) + np.pi/2.
+                # offset to make new_alpha, so that if car is head facing the camera, new_alpha = pi
+                # , and if car is back facing the camera, new_alpha = 0
+                new_alpha = float(line[3]) + np.pi/2. 
+                # make new_alpha always >= 0
                 if new_alpha < 0:
                     new_alpha = new_alpha + 2.*np.pi
+                # make new_alpha always <= 2pi, equivalent to if new_alpha > 2.*np.pi: new_alpha = new_alpha - 2.*np.pi
                 new_alpha = new_alpha - int(new_alpha/(2.*np.pi))*(2.*np.pi)
 
                 obj = {'name':line[0],
@@ -55,14 +66,21 @@ def parse_annotation(label_dir, image_dir):
                        'new_alpha': new_alpha
                       }
                 
+                # calculate the moving average of each obj dims.
+                # accumulate the sum of each dims for each obj
+                # get the count of the obj, then times the current avg of dims, + current obj's dim
                 dims_avg[obj['name']]  = dims_cnt[obj['name']]*dims_avg[obj['name']] + obj['dims']
                 dims_cnt[obj['name']] += 1
+                # get the new average
                 dims_avg[obj['name']] /= dims_cnt[obj['name']]
 
                 all_objs.append(obj)
+    # I have now accumulated all objects into all_objs from kitti data
+                
     ###### flip data
     for obj in all_objs:
-        # Fix dimensions
+        
+        # Get the dimensions offset from average (basically zero centering the values)
         obj['dims'] = obj['dims'] - dims_avg[obj['name']]
 
         # Fix orientation and confidence for no flip
