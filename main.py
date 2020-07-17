935be2c65e40344ce0c4ab2992f59fb7e447b800
Raymond H. Tu <raymtu@gmail.com>
2020-07-17 17:16:09 -0400

    Update main.py
    Added switch for different orientation types.

--- main.py
+++ main.py
@@ -26,39 +26,25 @@ save_path = './model/'
 dims_avg = {'Cyclist': np.array([ 1.73532436,  0.58028152,  1.77413709]), 'Van': np.array([ 2.18928571,  1.90979592,  5.07087755]), 'Tram': np.array([  3.56092896,   2.39601093,  18.34125683]), 'Car': np.array([ 1.52159147,  1.64443089,  3.85813679]), 'Pedestrian': np.array([ 1.75554637,  0.66860882,  0.87623049]), 'Truck': np.array([  3.07392252,   2.63079903,  11.2190799 ])}
 
 
-#### Placeholder
-# list of input images for each obj
-inputs = tf.placeholder(tf.float32, shape = [None, 224, 224, 3])
-# list of dims for each obj
-d_label = tf.placeholder(tf.float32, shape = [None, 3])
-# list of (2,2) sin, cos values of each bin/sector (2 bins by default) for each obj
-o_label = tf.placeholder(tf.float32, shape = [None, BIN, 2])
-# list of confs of each bin/sector for each obj
-c_label = tf.placeholder(tf.float32, shape = [None, BIN])
 
 
-def parse_args():
-    """Parse input arguments."""
-    parser = argparse.ArgumentParser(description='3D bounding box')
-    parser.add_argument('--mode',dest = 'mode',help='train or test',default = 'test')
-    parser.add_argument('--image',dest = 'image',help='Image path')
-    parser.add_argument('--label',dest = 'label',help='Label path')
-    parser.add_argument('--box2d',dest = 'box2d',help='2D detection path')
-    parser.add_argument('--output',dest = 'output',help='Output path', default = './validation/result_2/')
-    parser.add_argument('--model',dest = 'model')
-    parser.add_argument('--gpu',dest = 'gpu',default= '0')
-    args = parser.parse_args()
 
-    return args
-
-
-def build_model():
-
-  #### build some layer 
-  def LeakyReLU(x, alpha):
+def build_model(orientation_type):
+    #### Placeholder
+    # list of input images for each obj
+    inputs = tf.placeholder(tf.float32, shape = [None, 224, 224, 3])
+    # list of dims for each obj
+    d_label = tf.placeholder(tf.float32, shape = [None, 3])
+    # list of (2,2) sin, cos values of each multibin sector (2 bins by default) for each obj
+    o_label = tf.placeholder(tf.float32, shape = [None, BIN, 2])
+    # list of confidences of each multibin sector for each obj
+    c_label = tf.placeholder(tf.float32, shape = [None, BIN])
+    
+    #### custom layers and loss functions
+    def LeakyReLU(x, alpha):
       return tf.nn.relu(x) - alpha * tf.nn.relu(-x)
 
-  def orientation_loss(y_true, y_pred):
+    def multibin_orientation_loss(y_true, y_pred):
       # Find number of anchors
       anchors = tf.reduce_sum(tf.square(y_true), axis=2)
       anchors = tf.greater(anchors, tf.constant(0.5))
@@ -70,12 +56,13 @@ def build_model():
 
       return tf.reduce_mean(loss)
 
-  #####
-  #Build Graph
-  with slim.arg_scope([slim.conv2d, slim.fully_connected],
+    #####
+    #Build Graph
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
                       activation_fn=tf.nn.relu,
                       weights_initializer=tf.truncated_normal_initializer(0.0, 0.01),
                       weights_regularizer=slim.l2_regularizer(0.0005)):
+    # VGG backbone
     net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
     net = slim.max_pool2d(net, [2, 2], scope='pool1')
     net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
@@ -88,46 +75,39 @@ def build_model():
     net = slim.max_pool2d(net, [2, 2], scope='pool5')
     conv5 = tf.contrib.layers.flatten(net)
 
-    #dimension = slim.fully_connected(conv5, 512, scope='fc7_d')
-    dimension = slim.fully_connected(conv5, 512, activation_fn=None, scope='fc7_d')
-    dimension = LeakyReLU(dimension, 0.1)
-    dimension = slim.dropout(dimension, 0.5, scope='dropout7_d')
-    #dimension = slim.fully_connected(dimension, 3, scope='fc8_d')
-    dimension = slim.fully_connected(dimension, 3, activation_fn=None, scope='fc8_d')
-    #dimension = LeakyReLU(dimension, 0.1)
-
-    #loss_d = tf.reduce_mean(tf.square(d_label - dimension))
-    loss_d = tf.losses.mean_squared_error(d_label, dimension)
-
-    #orientation = slim.fully_connected(conv5, 256, scope='fc7_o')
     orientation = slim.fully_connected(conv5, 256, activation_fn=None, scope='fc7_o')
     orientation = LeakyReLU(orientation, 0.1)
     orientation = slim.dropout(orientation, 0.5, scope='dropout7_o')
-    #orientation = slim.fully_connected(orientation, BIN*2, scope='fc8_o')
-    orientation = slim.fully_connected(orientation, BIN*2, activation_fn=None, scope='fc8_o')
-    #orientation = LeakyReLU(orientation, 0.1)
-    orientation = tf.reshape(orientation, [-1, BIN, 2])
-    orientation = tf.nn.l2_normalize(orientation, dim=2)
-    loss_o = orientation_loss(o_label, orientation)
-
-    #confidence = slim.fully_connected(conv5, 256, scope='fc7_c')
-    confidence = slim.fully_connected(conv5, 256, activation_fn=None, scope='fc7_c')
-    confidence = LeakyReLU(confidence, 0.1)
-    confidence = slim.dropout(confidence, 0.5, scope='dropout7_c')
-    confidence = slim.fully_connected(confidence, BIN, activation_fn=None, scope='fc8_c')
-    loss_c = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=c_label, logits= confidence))
-   
-    confidence = tf.nn.softmax(confidence)
-    #loss_c = tf.reduce_mean(tf.square(c_label - confidence))
-    #loss_c = tf.losses.mean_squared_error(c_label, confidence)
     
-    total_loss = 4. * loss_d + 8. * loss_o + loss_c
+    if orientation_type == 'multibin':
+        orientation = slim.fully_connected(orientation, BIN*2, activation_fn=None, scope='fc8_o')
+        orientation = tf.reshape(orientation, [-1, BIN, 2])
+        orientation = tf.nn.l2_normalize(orientation, dim=2)
+        loss_o = multibin_orientation_loss(o_label, orientation)
+        
+        confidence = slim.fully_connected(conv5, 256, activation_fn=None, scope='fc7_c')
+        confidence = LeakyReLU(confidence, 0.1)
+        confidence = slim.dropout(confidence, 0.5, scope='dropout7_c')
+        confidence = slim.fully_connected(confidence, BIN, activation_fn=None, scope='fc8_c')
+        loss_c = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=c_label, logits=confidence))
+
+        confidence = tf.nn.softmax(confidence)
+
+        total_loss = 4. * loss_d + 8. * loss_o + loss_c
+        
+    if orientation_type == 'tricosine':
+        # TODO
+    if orientation_type == 'alpha':
+        # TODO
+    if orientation_type == 'rotation_y':
+        # TODO
+    
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss)
-    
-    return dimension, orientation, confidence, total_loss, optimizer, loss_d, loss_o, loss_c
+
+    return dimension, orientation, confidence, total_loss, optimizer
 
 
-def train(image_dir, box2d_loc, label_dir):
+def train(image_dir, box2d_loc, label_dir, orientation_type):
 
     # load data & gen data
     all_objs = parse_annotation(label_dir, image_dir)
@@ -267,9 +247,23 @@ def test(model, image_dir, box2d_loc, box3d_loc):
                 line = ' '.join([str(item) for item in line]) +' '+ str(np.max(prediction[2][0]))+ '\n'
                 box3d.write(line)
 
- 
-
+                
 if __name__ == "__main__":
+    
+    def parse_args():
+        """Parse input arguments."""
+        parser = argparse.ArgumentParser(description='3D bounding box')
+        parser.add_argument('--mode', dest = 'mode', help='train or test', default = 'test')
+        parser.add_argument('--image', dest = 'image', help='Image path')
+        parser.add_argument('--label', dest = 'label', help='Label path')
+        parser.add_argument('--box2d', dest = 'box2d', help='2D detection path')
+        parser.add_argument('--output', dest = 'output', help='Output path', default = './validation/result_2/')
+        parser.add_argument('--model', dest = 'model', help='trained model path')
+        parser.add_argument('--gpu', dest = 'gpu', default= '0')
+        parser.add_argument('--orientation', dest = 'orientation_type', help='Orientation type: tricosine, multibin, or alpha', default= 'tricosine')
+        args = parser.parse_args()
+        return args
+    
     args = parse_args()
 
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
@@ -282,11 +276,11 @@ if __name__ == "__main__":
     if args.mode == 'train':
         if args.label is None:
             raise IOError(('Label not found.'.format(args.label)))
-
-        train(args.image, args.box2d, args.label)
+            
+        train(args.image, args.box2d, args.label, args.orientation_type)
     else:
         if args.model is None:
             raise IOError(('Model not found.'.format(args.model)))
 
-        test(args.model, args.image, args.box2d, args.output)
+        test(args.model, args.image, args.box2d, args.output, args.orientation_type)
 
