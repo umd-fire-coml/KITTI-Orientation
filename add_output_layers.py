586ddcfd05120d8c1a1f61eb5d2c6d2289189991
Raymond <hh2@umd.edu>
2020-10-09 14:02:28 -0400

    updated layer building

--- add_output_layers.py
+++ add_output_layers.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 from tf.keras import layers
 
 
+NUM_SECTORS = 2**3 # TODO import from ?
+
 def first_output_stage(backbone_layer, out_shape):
     y = layers.Dense(256)(backbone_layer)
     y = layers.LeakyReLU(0.1)(y)
@@ -24,12 +26,14 @@ def add_output_layers(orientation_output_type, backbone_layer):
         return o_layer, c_layer_pre, c_layer
 
     # If not multibin -> output first stage with different shapes
-    output_shape = [0,0]
+    output_shape = [NUM_SECTORS, 1] # orientation_output_type == 'rot_y_sectors' or orientation_output_type == 'alpha_sectors'
     if orientation_output_type == 'alpha': output_shape = [1, 1]
     if orientation_output_type == 'rot_y': output_shape = [1, 1]
-    if orientation_output_type == 'tricosine': output_shape = [TRICOSINE_BINS, 1]
-    if orientation_output_type == 'rot_y_sectors': output_shape = [NUM_SECTORS, 1]
-    if orientation_output_type == 'alpha_sectors': output_shape = [NUM_SECTORS, 1]
+    if orientation_output_type == 'tricosine': output_shape = [3, 1]
     
     o_layer = first_output_stage(backbone_layer, output_shape)
+    
+    if (orientation_output_type == 'rot_y_sectors' or orientation_output_type == 'alpha_sectors'): 
+        o_layer = layers.Softmax(o_layer)
+    
     return o_layer
